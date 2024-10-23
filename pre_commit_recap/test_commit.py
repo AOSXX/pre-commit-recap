eab240bf1fedb3bdf64576756956da46bddeb0e6
jerome drescig <jeromedrescig@gmail.com>
2024-10-23 17:56:10 +0200

    testing

--- pre_commit_recap/test_commit.py
+++ pre_commit_recap/test_commit.py
@@ -1,6 +1,10 @@
-def greet(name: int) -> str:
+def greet(name: str) -> str:
     print("Hello, " + name + "!")
-    return 42
+    return f"Hello, {name}!"
 
 
-greet("world")
+# 2: error: Unsupported operand types for + ("str" and "int")  [operator]
+# 3: error: Incompatible return value type (got "int", expected "str")  [return-value]
+# 7: error: Argument 1 to "greet" has incompatible type "str"; expected "int"  [arg-type]
+if __name__ == "__main__":
+    greet("world")
